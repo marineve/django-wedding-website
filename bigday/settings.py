faa6adcae64c728dffa20b2d3c46bd2efed8b84b
marin.evergreen <marin.evergreen@workday.com>
2018-10-28 01:28:06 -0700

    Added basic login/logout functionality

--- bigday/settings.py
+++ bigday/settings.py
@@ -117,6 +117,9 @@ USE_L10N = True
 
 USE_TZ = True
 
+# Auth redirects
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
--- bigday/urls.py
+++ bigday/urls.py
@@ -1,10 +1,15 @@
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
+from django.views.generic.base import TemplateView
 
 
 urlpatterns = [
     url(r'^', include('wedding.urls')),
     url(r'^', include('guests.urls')),
     url(r'^admin/', admin.site.urls),
-    url('^accounts/', include('django.contrib.auth.urls'))
+    url('^accounts/', include('django.contrib.auth.urls')),
+    url(r'^login/$', auth_views.login, name='login'),
+    url(r'^logout/$', auth_views.logout, name='logout'),
+    url('', TemplateView.as_view(template_name='home.html'), name='home'),
 ]
